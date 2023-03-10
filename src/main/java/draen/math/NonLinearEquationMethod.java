13175285c9d15eaa9cb7326b11e867daa9eed58f
Draen <haykin.oleg@rambler.ru>
2023-03-10 21:21:50 +0300

    oops

--- src/main/java/draen/math/NonLinearEquationMethod.java
+++
@@ -1,8 +0,0 @@
-package draen.math;
-
-import draen.data.math.nonlinear.singular.NonLinearEquation;
-import draen.data.math.nonlinear.singular.NonLinearSolution;
-
-public interface NonLinearEquationMethod {
-    NonLinearSolution solve(NonLinearEquation equation);
-}
